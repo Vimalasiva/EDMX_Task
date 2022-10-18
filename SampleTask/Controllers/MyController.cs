aeed8f761223705bf4ec5f0349e20f6fe2024f71
STS665-VIMALA S <vimalasivashanmugam1@gmail.com>
2022-10-18 16:27:39 +0530

    Add project files.

---
+++ SampleTask/Controllers/MyController.cs
@@ -0,0 +1,37 @@
+﻿using SampleTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace SampleTask.Controllers
+{
+    public class MyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Start(MyModel model)
+        {
+            using (var set= new New_DatabaseEntities3())
+            {
+                Employee_Detail db = new Employee_Detail();
+                db.Employee_ID = model.EmployeeID;
+                db.Employee_Name = model.EmployeeName;
+                db.Role = model.Role;
+                db.Experience = model.Experience;
+                set.Employee_Detail.Add(db);
+                set.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ SampleTask/Models/MyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleTask.Models
+{
+    public class MyModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Role { get; set; }
+        public int Experience { get; set; }
+    }
+}
